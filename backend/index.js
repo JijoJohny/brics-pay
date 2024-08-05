8fd01c35a5a31b8398698f6b14ee317f151acdd6
thejzain <thejzainkp@gmail.com>
2024-08-05 11:43:44 +0530

    signup signin

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const authroute = require("./routes/auth");
 const app = express();
 const port = 3000;
 
+app.use(bodyparser.json());
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Brics API");
 });
+
+app.use("/api/user", authroute);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
