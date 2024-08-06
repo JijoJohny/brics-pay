661ac9c38badcbc79fcc83d742c5d09952754d43
thejzain <thejzainkp@gmail.com>
2024-08-06 18:04:38 +0530

    added res for buying

--- backend/controllers/buytoken.js
+++ backend/controllers/buytoken.js
@@ -10,6 +10,7 @@ exports.buytoken = async (req, res) => {
   try {
     const userpublic = req.user.public;
     await payment(ISSUER_SECRET, userpublic, amount);
+    return res.status(200).json({ message: "Transaction successful" });
   } catch (err) {
     console.error("Error purchasing token:", err);
     res.status(500).json({ error: "Failed to purchase token" });
