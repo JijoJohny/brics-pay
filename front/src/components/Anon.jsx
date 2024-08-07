05c587e569e396e9c0b46f6ac188a1d8dbf99ada
Yadhukrishnan <ybc2204@gmail.com>
2024-08-07 13:10:10 +0530

    anon_2

--- front/src/components/Anon.jsx
+++ front/src/components/Anon.jsx
@@ -1,74 +1,75 @@
-import { AnonAadhaarProof, LaunchProveModal, LogInWithAnonAadhaar, useAnonAadhaar } from '@anon-aadhaar/react'
-import { useEffect, useState } from 'react'
+import { LaunchProveModal, useAnonAadhaar } from '@anon-aadhaar/react';
+import { useEffect, useState } from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Alert from '@mui/material/Alert';
+
 
 const Anon = () => {
+  const [anonAadhar] = useAnonAadhaar();
+  
+  const [log, setLog] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-    const [anonAadhar] = useAnonAadhaar();
-    const [title , setTitle] = useState("")
-    const [log,setLog] = useState(false)
-    const setFunc = () =>
-    {
-      setTitle("Logout");
-      setLog(true)
+  
+
+  useEffect(() => {
+    console.log("Anon Status: ", anonAadhar.status);
+    anonAadhar.status === "logged-in" && setLog(true);
+    console.log(log);
+    const a = anonAadhar.anonAadhaarProofs;
+    if (a) {
+      console.log(a);
+      const parsedData = JSON.parse(a["0"].pcd);
+      const gender = parsedData.claim.gender;
+      const above = parsedData.claim.ageAbove18;
+      console.log("Gender:", gender);
+      console.log("Above 18:", above);
     }
-    useEffect(()=>
-    {
-        console.log("Anon Status: ",anonAadhar.status);
-        anonAadhar.status == "logged-out" || anonAadhar.status == "logging-in" ? setTitle("Login") : setFunc()
-        console.log(log)
-        const a = anonAadhar.anonAadhaarProofs;
-        if(a){
-          console.log(a);
-          
-          const parsedData = JSON.parse(a["0"].pcd);
-        const gender = parsedData.claim.gender;
-        const pincode = parsedData.claim.pincode;
-        console.log("Gender:", gender);
-        console.log("Pincode:", pincode);
-        }
-           
-    }, [anonAadhar,log]);
+  }, [anonAadhar, log]);
 
+  useEffect(() => {
+    if (log) {
+      setShowAlert(true);
+      const timer = setTimeout(() => {
+        setShowAlert(false);
+      }, 3000); // 3 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [log]);
 
   return (
     <div className='text-white absolute right-0'>
-        {/* <LogInWithAnonAadhaar 
-        nullifierSeed="113127483288210213123711461142312541791634"
-          fieldsToReveal={["revealAgeAbove18", "revealPinCode","revealGender","revealState"]} 
-  /> */}
-  <div className='mr-4'>
-      {!log &&(
-        
-        <LaunchProveModal
-        
-        buttonStyle={{ backgroundColor: '#9333ea', color: 'white' }}
-        buttonTitle={title}
-        nullifierSeed = "113127483288210213123711461142312541791634"
-        fieldsToReveal={["revealAgeAbove18", "revealPinCode","revealGender","revealState"]}
-        
-      />)
-      
-      }
-      {
-        log &&(
-          <div className='flex justify-end items-center hover:cursor-pointer'>
-          
-          <AccountCircleIcon fontSize='large'/>
+      {log !== true ? (
+        <div className=''>
+          <LaunchProveModal
+            buttonStyle={{ backgroundColor: '#9333ea', color: 'white' }}
+            buttonTitle="Verify"
+            nullifierSeed="113127483288210213123711461142312541791634"
+            fieldsToReveal={["revealAgeAbove18", "revealGender"]}
+          />
+        </div>
+      ) : (
+        <div className='flex justify-end items-center hover:cursor-pointer'>
+          <AccountCircleIcon fontSize='large' />
           <p className='text-lg'>✅</p>
-          </div>
-        )
-      }
-    </div>
-            {/* <p>status: {anonAadhar?.status}</p> */}
-            <div >
-      {/* Render the proof if generated and valid */}
-      
-    </div>
+        </div>
+      )}
+
+      {showAlert && (
+        <Alert severity="success" variant='outlined' className='absolute right-1/2'>
+          Login Successful
+        </Alert>
+      )}
     </div>
   );
 }
 
-export default Anon
+export default Anon;
 
 
+
+   {/* <LogInWithAnonAadhaar 
+        nullifierSeed="113127483288210213123711461142312541791634"
+          fieldsToReveal={["revealAgeAbove18", "revealPinCode","revealGender","revealState"]} 
+  /> */}
